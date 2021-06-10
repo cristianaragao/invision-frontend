4c13b11adfb496490c8bde7980ef9e8f8b479889
Cristian Aragão <cristian.oliveira@cits.br>
2021-06-10 19:36:27 -0400

    updated two

--- src/specs/SignIn.test.js
+++ src/specs/SignIn.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render } from "@testing-library/react";
 
-import SignIn from "../components/SignIn/";
+import SignIn from "../signin";
 
 test("Rendering Email field correctly.", () => {
     const { queryByLabelText, queryAllByPlaceholderText} = render(<SignIn />)
--- src/specs/SignUp.test.js
+++ src/specs/SignUp.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import {render} from "@testing-library/react";
 
-import SignUp from "../components/SignUp/";
+import SignUp from "../signup";
 
 
 test("Rendering Name field correctly.", () => {
